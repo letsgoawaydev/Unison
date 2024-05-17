a1fabe75f2b1e1b39b5c6b3e222393f57588c469
Ethan <ethpsa09@gmail.com>
2024-05-17 21:09:51 +0800

    fix offhand items code to work properly

--- src/main/java/dev/letsgoaway/unison/EventListener.java
+++ src/main/java/dev/letsgoaway/unison/EventListener.java
@@ -23,31 +23,32 @@ public class EventListener implements Listener {
 
     @EventHandler
     public void onQuickOffhandSwap(PlayerSwapHandItemsEvent ev) {
-        ev.setCancelled(Config.disableOffhandKeybind);
+        ev.setCancelled(Config.disableOffhandKeybind || !bedrockAllowed.contains(Objects.requireNonNull(ev.getOffHandItem()).getType()));
     }
 
-    public List<Material> bedrockAllowed = Arrays.asList(Material.SHIELD, Material.ARROW, Material.SPECTRAL_ARROW, Material.TIPPED_ARROW, Material.FIREWORK_ROCKET, Material.TOTEM_OF_UNDYING, Material.FILLED_MAP, Material.NAUTILUS_SHELL);
+    public List<Material> bedrockAllowed = Arrays.asList(Material.SHIELD, Material.ARROW, Material.SPECTRAL_ARROW, Material.TIPPED_ARROW, Material.FIREWORK_ROCKET, Material.TOTEM_OF_UNDYING, Material.FILLED_MAP, Material.NAUTILUS_SHELL, Material.AIR, Material.VOID_AIR, Material.CAVE_AIR);
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent ev) {
         if (ev.getClick().equals(ClickType.SWAP_OFFHAND)) {
             ev.setCancelled(Config.disableOffhandKeybind);
         }
+        if (ev.getClick().equals(ClickType.SWAP_OFFHAND) && Config.bedrockOffhandItemsOnly) {
+            ev.setCancelled(!bedrockAllowed.contains(Objects.requireNonNull(ev.getCurrentItem()).getType()));
+        }
         if (ev.getClick().equals(ClickType.NUMBER_KEY)) {
             ev.setCancelled(Config.disableNumberKeyInventoryHotkey);
         }
         if (Config.bedrockOffhandItemsOnly && !ev.isCancelled() && ev.getCursor() != null) {
             if (ev.getSlot() == 40 &&
-                    (
-                    ev.getAction().name().contains("PLACE")
+                    (ev.getAction().name().contains("PLACE")
                     || ev.getAction().equals(InventoryAction.SWAP_WITH_CURSOR)
-                    || ev.getClick().equals(ClickType.SWAP_OFFHAND)
                     || ev.getAction().equals(InventoryAction.NOTHING)
                     || ev.getAction().equals(InventoryAction.UNKNOWN)
                     )
                     && ev.getInventory().getType().equals(InventoryType.CRAFTING)
-                    && !bedrockAllowed.contains(ev.getCursor().getType())
-                    && !bedrockAllowed.contains(Objects.requireNonNull(ev.getCurrentItem()).getType())) {
+                    && (!bedrockAllowed.contains(ev.getCursor().getType())
+                    || !bedrockAllowed.contains(Objects.requireNonNull(ev.getCurrentItem()).getType()))) {
                 ev.setCancelled(true);
                 if (ev.getWhoClicked() instanceof Player player){
                     player.updateInventory();
