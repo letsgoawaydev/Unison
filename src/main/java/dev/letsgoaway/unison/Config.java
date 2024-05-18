b839c29a93a5c6826c8696273bd56fb3bd962701
Ethan <ethpsa09@gmail.com>
2024-05-18 17:50:51 +0800

    add shield block and bedrock consumable effects

--- src/main/java/dev/letsgoaway/unison/Config.java
+++ src/main/java/dev/letsgoaway/unison/Config.java
@@ -1,5 +1,6 @@
 package dev.letsgoaway.unison;
 
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.io.IOException;
@@ -13,6 +14,9 @@ public class Config {
     public static boolean disableNumberKeyInventoryHotkey = true;
 
     public static boolean bedrockOffhandItemsOnly = true;
+
+    public static boolean bedrockConsumableEffects = true;
+    public static boolean blockWhenSneaking = true;
     static FileConfiguration config;
 
     public static void loadConfig() {
@@ -22,6 +26,12 @@ public class Config {
         config = Unison.plugin.getConfig();
         loadValues();
         Unison.plugin.getDataFolder().toPath().resolve("config.yml").toFile().delete();
+        Unison.plugin.saveResource("config.yml", false);
+        try {
+            Unison.plugin.getConfig().load(Unison.plugin.getDataFolder().toPath().resolve("config.yml").toFile());
+        } catch (IOException | InvalidConfigurationException e) {
+
+        }
         saveValues();
     }
 
@@ -50,6 +60,12 @@ public class Config {
         if (config.contains("bedrock-offhand-items-only", true)) {
             bedrockOffhandItemsOnly = config.getBoolean("bedrock-offhand-items-only");
         }
+        if (config.contains("bedrock-consumable-effects", true)) {
+            bedrockConsumableEffects = config.getBoolean("bedrock-consumable-effects");
+        }
+        if (config.contains("allow-block-only-when-sneaking", true)) {
+            blockWhenSneaking = config.getBoolean("allow-block-only-when-sneaking");
+        }
     }
 
     public static void saveValues() {
@@ -58,6 +74,8 @@ public class Config {
         config.set("disable-offhand-keybind", disableOffhandKeybind);
         config.set("disable-number-key-inventory-hotkey", disableNumberKeyInventoryHotkey);
         config.set("bedrock-offhand-items-only", bedrockOffhandItemsOnly);
+        config.set("bedrock-consumable-effects", bedrockConsumableEffects);
+        config.set("allow-block-only-when-sneaking", blockWhenSneaking);
         try {
             config.save(Unison.plugin.getDataFolder().toPath().resolve("config.yml").toFile());
         } catch (IOException e) {
--- src/main/java/dev/letsgoaway/unison/EventListener.java
+++ src/main/java/dev/letsgoaway/unison/EventListener.java
@@ -1,19 +1,26 @@
 package dev.letsgoaway.unison;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.inventory.*;
-import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class EventListener implements Listener {
+    private HashMap<UUID, Material> lastEaten = new HashMap<>();
+
     @EventHandler
     public void onEntitySpawn(EntitySpawnEvent ev) {
         if (Config.enableArmsOnArmorStands && ev.getEntity() instanceof ArmorStand armorStand) {
@@ -30,6 +37,18 @@ public class EventListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent ev) {
+        try {
+            if (ev.getInventory().getItem(0).getItemMeta().getLore().get(0).equals("Cancelling shield...")) {
+                ev.setCancelled(true);
+                Tick.runIn(1L,()->{
+                   if (ev.getWhoClicked() instanceof Player player){
+                       player.updateInventory();
+                   }
+                });
+                return;
+            }
+        } catch (NullPointerException ignored) {
+        }
         if (ev.getClick().equals(ClickType.SWAP_OFFHAND)) {
             ev.setCancelled(Config.disableOffhandKeybind);
             return;
@@ -44,15 +63,15 @@ public class EventListener implements Listener {
         if (Config.bedrockOffhandItemsOnly && !ev.isCancelled() && ev.getCursor() != null) {
             if (ev.getSlot() == 40 &&
                     (ev.getAction().name().contains("PLACE")
-                    || ev.getAction().equals(InventoryAction.SWAP_WITH_CURSOR)
-                    || ev.getAction().equals(InventoryAction.NOTHING)
-                    || ev.getAction().equals(InventoryAction.UNKNOWN)
+                            || ev.getAction().equals(InventoryAction.SWAP_WITH_CURSOR)
+                            || ev.getAction().equals(InventoryAction.NOTHING)
+                            || ev.getAction().equals(InventoryAction.UNKNOWN)
                     )
                     && ev.getInventory().getType().equals(InventoryType.CRAFTING)
                     && (!bedrockAllowed.contains(ev.getCursor().getType())
                     || !bedrockAllowed.contains(Objects.requireNonNull(ev.getCurrentItem()).getType()))) {
                 ev.setCancelled(true);
-                if (ev.getWhoClicked() instanceof Player player){
+                if (ev.getWhoClicked() instanceof Player player) {
                     player.updateInventory();
                 }
             }
@@ -60,16 +79,121 @@ public class EventListener implements Listener {
     }
 
     @EventHandler
-    public void onInventoryDrag(InventoryDragEvent ev){
+    public void onInventoryDrag(InventoryDragEvent ev) {
         if (Config.bedrockOffhandItemsOnly
                 && ev.getInventory().getType().equals(InventoryType.CRAFTING)
-                && !bedrockAllowed.contains(ev.getOldCursor().getType())){
-            ev.getRawSlots().forEach((slot)->{
-                if (slot == 45){
+                && !bedrockAllowed.contains(ev.getOldCursor().getType())) {
+            ev.getRawSlots().forEach((slot) -> {
+                if (slot == 45) {
                     ev.setCancelled(true);
                 }
             });
         }
+    }
 
+    public void stopShieldBlocking(Player player) {
+
+    }
+
+    @EventHandler
+    public void onPlayerSneak(PlayerToggleSneakEvent ev) {
+        if (!Config.blockWhenSneaking){
+            return;
+        }
+        if (ev.isSneaking()) {
+            ev.getPlayer().setCooldown(Material.SHIELD, 0);
+        } else {
+            ev.getPlayer().setCooldown(Material.SHIELD, 999999999);
+            if (ev.getPlayer().isBlocking()
+                    || ev.getPlayer().getInventory().getItemInOffHand().getType().equals(Material.SHIELD)
+                    || ev.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.SHIELD)
+            ) {
+                Inventory inventory = Bukkit.createInventory(null, InventoryType.STONECUTTER, " ");
+                ItemStack itemStack = new ItemStack(Material.COBBLED_DEEPSLATE);
+                ItemMeta meta = itemStack.getItemMeta();
+                if (meta == null)
+                    return;
+                meta = meta.clone();
+                meta.setLore(List.of("Cancelling shield..."));
+                itemStack.setItemMeta(meta);
+                inventory.addItem(itemStack);
+                ev.getPlayer().openInventory(inventory);
+                Tick.runIn(0L, () -> {
+                    ev.getPlayer().closeInventory();
+                });
+            }
+        }
+    }
+
+    private void disconnect(UUID player) {
+        lastEaten.remove(player);
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent ev) {
+        disconnect(ev.getPlayer().getUniqueId());
+    }
+
+    @EventHandler
+    public void onPlayerKick(PlayerKickEvent ev) {
+        disconnect(ev.getPlayer().getUniqueId());
+    }
+
+    @EventHandler
+    public void onPlayerItemConsume(PlayerItemConsumeEvent ev) {
+        lastEaten.putIfAbsent(ev.getPlayer().getUniqueId(), ev.getItem().getType());
+        lastEaten.replace(ev.getPlayer().getUniqueId(), ev.getItem().getType());
+    }
+
+    @EventHandler
+    public void onPotionEffect(EntityPotionEffectEvent ev) {
+        if (!Config.bedrockConsumableEffects) {
+            return;
+        }
+        if (!ev.getCause().equals(EntityPotionEffectEvent.Cause.FOOD) && !ev.getCause().equals(EntityPotionEffectEvent.Cause.TOTEM)) {
+            return;
+        }
+        if (ev.getEntity() instanceof Player player) {
+            Material lastFood = lastEaten.get(player.getUniqueId());
+            PotionEffect effect = ev.getNewEffect();
+            if (effect == null) {
+                return;
+            }
+            Unison.plugin.getLogger().info(effect.toString());
+            PotionEffectType type = effect.getType();
+            int duration = effect.getDuration();
+            int amplifier = effect.getAmplifier();
+            boolean ambient = effect.isAmbient();
+            boolean particles = effect.hasParticles();
+            boolean icon = effect.hasIcon();
+
+            if (ev.getCause().equals(EntityPotionEffectEvent.Cause.FOOD)) {
+                if (lastFood.equals(Material.ENCHANTED_GOLDEN_APPLE)) {
+                    if (type.equals(PotionEffectType.REGENERATION) && duration == 400 && amplifier == 1) {
+                        ev.setCancelled(true);
+                        player.addPotionEffect(
+                                new PotionEffect(PotionEffectType.REGENERATION, 600, 4, ambient, particles, icon)
+                        );
+                    }
+                }
+                /*
+                if (lastFood.equals(Material.SUSPICIOUS_STEW)) {
+                    if (type.equals(PotionEffectType.FIRE_RESISTANCE) && duration == 400 && amplifier == 1) {
+                        ev.setCancelled(true);
+                        player.addPotionEffect(
+                                new PotionEffect(PotionEffectType.REGENERATION, 600, 4, ambient, particles, icon)
+                        );
+                    }
+                }
+                */
+            } else if (ev.getCause().equals(EntityPotionEffectEvent.Cause.TOTEM)) {
+                if (type.equals(PotionEffectType.REGENERATION) && duration == 80 && amplifier == 1) {
+                    ev.setCancelled(true);
+                    player.addPotionEffect(
+                            new PotionEffect(PotionEffectType.REGENERATION, 800, amplifier, ambient, particles, icon)
+                    );
+                }
+            }
+        }
     }
 }
